af5d3217390557f66f8f96179e4ddc7e22587a9d
Jezebel1990 <jezebelguedes@gmail.com>
2023-03-09 00:03:06 -0300

    add desafio 02

--- classes/class04/pages/titles/src/util/gestures.js
+++ classes/class04/pages/titles/src/util/gestures.js
@@ -1,7 +1,8 @@
-const { GestureDescription, Finger, FingerCurl } = window.fp
+const { GestureDescription, Finger, FingerCurl, FingerDirection } = window.fp
 
 const ScrollDownGesture = new GestureDescription('scroll-down')  // ✊️
 const ScrollUpGesture = new GestureDescription('scroll-up') // 🖐
+const ThumbsUpGesture = new GestureDescription('thumbs_up') //👍
 const ClickGesture = new GestureDescription('click') // 🤏🏻
 
 // Scroll Down
@@ -27,6 +28,35 @@ for (let finger of Finger.all) {
   ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
 }
 
+
+// ThumbUp
+// - curl: none (must)
+// - direction vertical up (best)
+// - direction diagonal up left / right (acceptable)-----------------------------------------------------------------------------
+ThumbsUpGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
+ThumbsUpGesture.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
+ThumbsUpGesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 0.9);
+ThumbsUpGesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 0.9);
+
+// all other fingers:
+// - curled (best)
+// - half curled (acceptable)
+// - pointing down is NOT acceptable
+for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+  ThumbsUpGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+  ThumbsUpGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
+}
+
+// require the index finger to be somewhat left or right pointing
+// but NOT down and NOT fully up
+ThumbsUpGesture.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 1.0);
+ThumbsUpGesture.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0);
+ThumbsUpGesture.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0);
+ThumbsUpGesture.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 1.0);
+
+
+
+
 // Click
 // -----------------------------------------------------------------------------
 ClickGesture.addCurl(Finger.Index, FingerCurl.HalfCurl, 0.8)
@@ -48,12 +78,14 @@ ClickGesture.addCurl(Finger.Pinky, FingerCurl.FullCurl, 0.9)
 const knownGestures = [
   ScrollDownGesture,
   ScrollUpGesture,
+  ThumbsUpGesture,
   ClickGesture,
 ]
 
 const gestureStrings = {
   'scroll-up': '🖐',
   'scroll-down': '✊️',
+  'thumbs_up': '👍',
   'click': '🤏🏻'
 }
 
